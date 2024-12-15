5d76b9fdf85b088872dfa35ef88fbb509b03e7d0
wigor <wigor21@hotmail.com>
2024-12-15 18:36:05 -0300

    feat: created integration with api auth sign in

--- apps/web/src/app/(auth)/sign-in/components/SignInForm.tsx
+++ apps/web/src/app/(auth)/sign-in/components/SignInForm.tsx
@@ -1,19 +1,36 @@
 "use client";
 
 import Link from "next/link";
-
-import { Button } from "@/components/ui/button";
+import { redirect } from "next/navigation";
 
 import { SignInFormData, useSignInForm } from "@/app/(auth)/sign-in/schema";
-import { EmailFormField } from "@/components/EmailFormField";
+
+import { Button } from "@/components/ui/button";
 import { Form } from "@/components/ui/form";
+
+import { EmailFormField } from "@/components/EmailFormField";
 import { PasswordFormField } from "@/components/PasswordFormField";
+
+import { signIn } from "@/app/(auth)/sign-in/actions";
+import { useState } from "react";
 
 export const SignInForm = () => {
   const form = useSignInForm();
+  const [errorMessage, setErrorMessage] = useState<string | null>(null);
 
-  const onSubmit = (data: SignInFormData) => {
-    console.log(data);
+  const onSubmit = async (data: SignInFormData) => {
+    try {
+      setErrorMessage(null);
+      await signIn(data);
+    } catch (error) {
+      if (error instanceof Error) {
+        return setErrorMessage(error.message);
+      }
+
+      return setErrorMessage("Erro na validação");
+    }
+
+    redirect("/admin");
   };
 
   return (
@@ -21,7 +38,12 @@ export const SignInForm = () => {
       <form onSubmit={form.handleSubmit(onSubmit)}>
         <div className="space-y-4">
           <EmailFormField control={form.control} name="email" />
+
           <PasswordFormField control={form.control} name="password" />
+
+          {errorMessage && (
+            <p className="mb-2 text-destructive">{errorMessage}</p>
+          )}
         </div>
 
         <Link
